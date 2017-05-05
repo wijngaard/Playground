acc290758018f9bd52ca5a5038d105be6e3c598e
Renny <wijngaard@users.noreply.github.com>
2017-05-05 16:19:52 +0200

    add Test Case for Ireland

--- src/test/java/eu/sig/training/ch03/FlagsTest.java
+++ src/test/java/eu/sig/training/ch03/FlagsTest.java
@@ -10,7 +10,7 @@ import org.junit.Test;
 public class FlagsTest {
 
     @Test
-    public void testGetFlag() {
+    public void testGetDucthFlag() {
         FlagFactory factory = new FlagFactory();
 
         List<Color> colors = factory.getFlagColors(Nationality.DUTCH);
@@ -19,5 +19,16 @@ public class FlagsTest {
         assertEquals(Color.WHITE, colors.get(1));
         assertEquals(Color.BLUE, colors.get(2));
     }
+    
+    @Test
+    public void testGetIrelandFlag() {
+        FlagFactory factory = new FlagFactory();
 
+        List<Color> colors = factory.getFlagColors(Nationality.IRELAND);
+
+        assertEquals(Color.GREEN, colors.get(0));
+        assertEquals(Color.WHITE, colors.get(1));
+        assertEquals(Color.ORANGE, colors.get(2));
+    }
+    
 }
